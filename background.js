774f81723bd8b5dd73c310bb008baba80785064d
Faruukko <ilian.allaf@outlook.fr>
2023-01-18 19:16:29 +0100

    Messages Features

--- background.js
+++ background.js
@@ -1,35 +1,62 @@
 let cptLike = 0;
 let cptPass = 0;
+let cptSentMessages = 0;
+let cptReceivedMessages = 0;
 
-
-function getStoredCptValues()
+function getStoredSwipeValues()
 {
     chrome.storage.local.get(['cptLike', 'cptPass'], function(result) {
         cptLike = result.cptLike || 0;
         cptPass = result.cptPass || 0;
+        chrome.runtime.sendMessage({type: "UpdateHtmlSwipeValues", cptLike: cptLike, cptPass: cptPass});
     });
-    chrome.runtime.sendMessage({type: "onSwipe", cptLike: cptLike, cptPass: cptPass});
+}
+function getStoredMessageValues()
+{
+    chrome.storage.local.get(['cptSentMessages', 'cptReceivedMessages'], function(result) {
+        cptSentMessages = result.cptSentMessages || 0;
+        cptReceivedMessages = result.cptReceivedMessages || 0;
+        chrome.runtime.sendMessage({type: "UpdateHtmlMessageValues", cptSentMessages: cptSentMessages, cptReceivedMessages: cptReceivedMessages});
+    });
 }
 
+
 function requestHandler(req){
+    //Likes
     if(req.url.includes("https://api.gotinder.com/like/") && req.method == "POST"){
         cptLike ++;
         chrome.storage.local.set({cptLike: cptLike});
-        getStoredCptValues();
+        getStoredSwipeValues();
     }
+    //Pass
     else if(req.url.includes("https://api.gotinder.com/pass/") && req.method == "GET"){
         cptPass ++;
         chrome.storage.local.set({cptPass: cptPass});
-        getStoredCptValues();
+        getStoredSwipeValues();
+    }
+    //Sent Messages
+    else if(req.url.includes("https://api.gotinder.com/user/matches/") && req.method == "POST"){
+        console.log("Sent Messages")
+        cptSentMessages ++;
+        chrome.storage.local.set({cptSentMessages: cptSentMessages});
+        getStoredMessageValues();
+    }
+    //Received Messages
+    else if(req.url.includes("POST https://api.gotinder.com/updates?locale=fr") && req.method == "POST"){
+        console.log("Received Messages")
+        cptSentMessages ++;
+        chrome.storage.local.set({cptReceivedMessages: cptReceivedMessages});
+        getStoredMessageValues();
     }
 }
 
+//#region Listener
 chrome.runtime.onInstalled.addListener(() => {
     console.log("EXTENSION INSTALLED");
 });
 
 chrome.webNavigation.onDOMContentLoaded.addListener(() => {
-    console.log("NOUVELLE PAGE OUVERTE")
+    console.log("NEW PAGE")
 })
 
 chrome.webRequest.onBeforeSendHeaders.addListener(requestHandler,
@@ -38,10 +65,18 @@ chrome.webRequest.onBeforeSendHeaders.addListener(requestHandler,
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        if( request.type === "getStoredCptValues" ) {
-            getStoredCptValues();
+        if( request.type === "getStoredSwipeValues" ) {
+            getStoredSwipeValues();
         }
     });
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if( request.type === "getStoredMessageValues" ) {
+            getStoredMessageValues();
+        }
+    });
+//#endregion
+
 /*
 Faire stat journaliere et stat totale
 likes, pass, swipes, match/likes, temps passé, nombre de message envoyé, nombre de message recus
--- content.js
+++ content.js
@@ -1,19 +1,39 @@
-chrome.runtime.sendMessage({type: "getStoredCptValues"});
+chrome.runtime.sendMessage({type: "getStoredSwipeValues"});
+chrome.runtime.sendMessage({type: "getStoredMessageValues"});
 
 likes  = document.getElementById('likesId');
 pass =  document.getElementById("passId");
 swipes = document.getElementById("swipesId");
+sentMessages = document.getElementById("sentMessagesId");
+receivedMessages = document.getElementById("receivedMessagesId");
 
-function onSwipe(cptLike, cptPass){
-    console.log("onSwipe Methode")
+function UpdateHtmlSwipeValues(cptLike, cptPass){
     likes.innerHTML = cptLike.toString();
     pass.innerHTML = cptPass.toString();
     swipes.innerHTML = (cptLike + cptPass).toString();
 }
 
+function UpdateHtmlMessageValues(cptSentMessages, cptReceivedMessages){
+    sentMessages.innerHTML = cptSentMessages.toString();
+    receivedMessages.innerHTML = cptReceivedMessages.toString();
+}
+
+//#region Listener
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        if( request.type === "onSwipe" ) {
-            onSwipe(request.cptLike, request.cptPass);
+        if( request.type === "UpdateHtmlMessageValues" ) {
+            UpdateHtmlMessageValues(request.cptSentMessages, request.cptReceivedMessages);
         }
     });
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if( request.type === "UpdateHtmlSwipeValues" ) {
+            UpdateHtmlSwipeValues(request.cptLike, request.cptPass);
+        }
+    });
+//#endregion
+
+
+//envoyer un message: POST https://api.gotinder.com/user/matches/634470e5cd3ff90100e87dd0638b27ebb0ef270100631eae?locale=fr
+//recevoir un message: POST https://api.gotinder.com/updates?locale=fr
